cd1cbe68d72a7b72c845aa741ef41766c2102b85
Kai Weeks <kai.weeks@gmail.com>
2020-11-13 19:00:09 +0000

    Adds foundation css and js to project entry

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import './../node_modules/foundation-sites/dist/css/foundation.min.css';
+import './../node_modules/foundation-sites/dist/js/foundation.min.js';
+
+
 
 createApp(App)
   .use(store)
