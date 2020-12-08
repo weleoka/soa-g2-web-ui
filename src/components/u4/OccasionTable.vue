43c6c82aca09208a9030bc193f39a06bed83ec77
Kai Weeks <kai.weeks@gmail.com>
2020-12-08 23:10:23 +0000

    Basics for occasiontable component

--- src/components/u4/OccasionTable.vue
+++ src/components/u4/OccasionTable.vue
@@ -1,22 +1,52 @@
 <template>
-  <div style="height: 250px">
-    <VueCal />
+  <div class="table-responsive py-4">
+    <table class="table table-striped table-hover" id="occasion-table">
+      <thead class="thead-light table-success">
+        <tr>
+          <th>Välj</th>
+          <th scope="col">Tillfällekod</th>
+          <th scope="col">Plats</th>
+          <th scope="col">Period</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>im a checkbox</td>
+          <td>placeholder</td>
+          <td>placeholder</td>
+          <td>placeholder</td>
+        </tr>
+        <tr
+          @click="$emit('select-occasion-event', occasion.id)"
+          v-for="(occasion, i) in occasionArr"
+          :key="i"
+        >
+          <td>{{ occasion.occasionCode }}</td>
+          <td>{{ occasion.location }}</td>
+          <td>{{ occasion.period }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <button type="button" @click="$emit('refresh-occasions-event')">
+      Refresh tillfällen
+    </button>
   </div>
 </template>
 
 <script>
-import VueCal from "vue-cal";
-import "vue-cal/dist/vuecal.css";
+"use strict";
 
 export default {
   name: "OccasionTable",
-  components: { VueCal },
-  data: () => ({
-
-  }),
-}
+  components: {},
+  data() {
+    return {
+      checkedOccasions: [],
+      occasionArr: []
+    };
+  },
+  methods: {}
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/views/u4/Scheduleworks.vue
+++ src/views/u4/Scheduleworks.vue
@@ -1,28 +1,45 @@
 /* EA & SOA Group 2 HT2020. The idea for this is that it is a "wrapper" component that
 includes the "menu" for the schedule management parts */
+
 <template>
-  <div id="home">
-    <OccasionTable />
+  <div class="container" id="schedule-works">
+    <OccasionTable
+      @refresh-occasions-event="refreshOccasions"
+      @select-occasion-event="fetchOccasionDetails"
+    />
+    <VueCal />
   </div>
 </template>
 
-<script lang="ts">
-import { Options, Vue } from "vue-class-component";
+<script>
+"use strict";
 
-import { mapActions } from "vuex";
+import { Options, Vue } from "vue-class-component";
+import VueCal from "vue-cal";
 import OccasionTable from "@/components/u4/OccasionTable.vue";
+import "vue-cal/dist/vuecal.css";
 
 @Options({
   components: {
-    OccasionTable
+    OccasionTable,
+    VueCal
+  },
+  data() {
+    return {
+      checkedNames: [],
+      state: {
+        allModulesMode: false // Show or hide the courseCode column in table
+      }
+    };
   },
   methods: {
-    ...mapActions([]), // remove
-    getModuleDetails(moduleId: string) {
-      console.log("select-module-event triggered data: " + moduleId);
-      this.$router.push({ name: "moduleview", params: { moduleId } });
-      // The above translates to being the same as:
-      //this.$router.push({ name: "moduleview", params: { moduleId: moduleId } });
+    // Reload the occasions table. Mostly for testing requests.
+    refreshOccasions() {
+      console.log("refreshOccasions() NOT implemented.");
+    },
+    // Get meta info about an occasion... maybe for a popup or sidepanel infowindow.
+    fetchOccasionDetails() {
+      console.log("fetchOccasionDetails() NOT implemented");
     }
   }
 })
