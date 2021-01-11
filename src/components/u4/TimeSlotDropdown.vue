a629cb56acf89bbef00a8b7387177b697feff88c
Kai Weeks <kai.weeks@gmail.com>
2021-01-11 19:22:06 +0000

    adds new file for dropdown select for time slots

---
+++ src/components/u4/TimeSlotDropdown.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <label>
+      Tid
+      <select
+        @change="$emit('selection-event', this.selected)"
+        v-model="selected"
+        ><option
+          v-for="(option, i) in options"
+          :key="i"
+          v-bind:value="option.id"
+        >
+          {{ option.text }}
+        </option>
+      </select>
+    </label>
+  </div>
+</template>
+
+<script lang="ts">
+import { mapState } from "vuex";
+import { Options, Vue } from "vue-class-component";
+import { Ut } from "@/service/utils";
+
+@Options({
+  name: "TimeSlotDropdown",
+  props: {
+    preselected: String
+  },
+  emits: ["selection-event"],
+  data() {
+    return {
+      selected: "",
+      options: []
+    };
+  },
+  computed: mapState("scheduleStore", ["timeSlots"]),
+  beforeMount() {
+    for (let i = 1; i < Object.keys(this.timeSlots).length + 1; i++) {
+      //Ut.pp(this.timeSlots[`${i}`]);
+      const slot = this.timeSlots[`${i}`];
+      const startStr: string = Ut.minutesToHoursAndMinutes(slot.from);
+      const endStr: string = Ut.minutesToHoursAndMinutes(slot.to);
+      this.options.push({
+        text: `${i}: ${startStr} - ${endStr}`,
+        id: i
+      });
+    }
+  },
+  methods: {
+    nearestTimeSlot: datetime => -1 // Fetch the timeslot nearest to where the user clicked.
+  }
+})
+export default class TimeSlotDropdown extends Vue {}
+</script>
+
+<style scoped></style>
